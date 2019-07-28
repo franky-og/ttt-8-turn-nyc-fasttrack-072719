36579064e7a809b3ace6e683841a9c4d0f675164
Franky Liu <cuifranky95@gmail.com>
2019-07-28 03:55:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+if index <= 8 && index >= 0 && !position_taken?(board, index)
+  true
+else
+  false
+end
+end
